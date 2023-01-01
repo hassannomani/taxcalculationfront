03504eda1ffa9d5629865bf6c6af3f20670cd037
redsaiyanAlpha <nomanialvi@gmail.com>
2023-01-02 00:46:16 +0600

    payment

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { LocalstorageService } from '../service/storage/localstorage.service';
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
   styleUrls: ['./payment.component.css']
 })
-export class PaymentComponent {
+export class PaymentComponent implements OnInit{
+  constructor(
+   
+    private storageServ : LocalstorageService
+    ){}
 
+    ngOnInit(): void {
+      
+    }
+    
+    paymentSubmit(){
+
+    }
 }
